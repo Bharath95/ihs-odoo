ad59a61b0dca4bcaf4d468f7a99ae726c5b405b4
RavirajAtIsha <raviraj.gardi@sadhguru.org>
2025-04-16 20:01:14 +0530

    Testing workflow

--- custom_addons/test_module/test.py
+++ custom_addons/test_module/test.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 # small_lint_sample.py
 
-import os, sys     # Multiple imports on one line (usually flagged)
 
 def add(x,y):      # Missing space after comma
     result=x+y     # Missing spaces around operator
-    return result  
+    return result
 
 # Unused variable
 unused = "This will be flagged"
