1e06f1b856e60254f660e4332d49ed6210b0a135
Bharath95 <o.bharath95@gmail.com>
2025-04-24 00:37:16 +0530

    export_code_test

---
+++ custom_addons/export_code/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Export Code",
+    "version": "1.0",
+    "category": "Tools",
+    "summary": "Export UI changes (views, fields) to module files",
+    "depends": ["base", "web"],
+    "data": [
+        "views/export_wizard_views.xml",
+    ],
+    "installable": true,
+    "application": false,
+}
--- custom_addons/hello/models/hello_model.py
+++ custom_addons/hello/models/hello_model.py
@@ -1,11 +1,9 @@
 from odoo import fields, models
 
 
-class helloModel(models.Model):
+class HelloModel(models.Model):
     _name = "hello.model"
     _description = "Hello Model"
 
     name = fields.Char(string="Name")
     message = fields.Char(string="Message", default="Hello, World!")
-
-    print("test lint")
